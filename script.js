d44b798f418f29a139f2f11211bf490f683de591
jorabhai <msd78075@gmail.com>
2026-05-23 09:19:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function resizeCanvas(){
 ========================= */
 
 const stars = [];
-const totalStars = 100;
+const totalStars = 80;
 
 // High-fidelity structural coordinate blueprints mapping the word: S - H - A - F - A
 const letterBlueprints = {
